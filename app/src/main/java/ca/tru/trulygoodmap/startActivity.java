886faf826b96cf6b2634a2243b32887868758c6e
Drew <31863814+DrewWeberDev@users.noreply.github.com>
2017-11-11 11:48:49 -0800

    added campus activity

--- app/src/main/java/ca/tru/trulygoodmap/startActivity.java
+++ app/src/main/java/ca/tru/trulygoodmap/startActivity.java
@@ -1,13 +1,27 @@
 package ca.tru.trulygoodmap;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class startActivity extends AppCompatActivity {
+public class startActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(startActivity.this, campus_activity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
+
     }
 }
