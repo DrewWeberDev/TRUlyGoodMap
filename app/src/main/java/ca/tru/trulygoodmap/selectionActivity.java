2d74c2ff94a4055d6792db59a626e538b4d369da
Drew <31863814+DrewWeberDev@users.noreply.github.com>
2017-11-18 19:02:39 -0800

    sync

--- app/src/main/java/ca/tru/trulygoodmap/selectionActivity.java
+++ app/src/main/java/ca/tru/trulygoodmap/selectionActivity.java
@@ -3,8 +3,6 @@ package ca.tru.trulygoodmap;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
